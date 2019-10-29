35ae0e9c2eb44007dcc78eee1bced0f9cf350058
Luz <zulsb2093@gmail.com>
2019-10-29 17:04:54 +0000

    Functions prints

--- prints.c
+++ prints.c
@@ -1,4 +1,12 @@
 #include "holberton.h"
+
+/**
+  *print_s - Function print string.
+  *@string: Argument string type va_list.
+  *Return: Nothing.
+  *
+  */
+
 void print_s(va_list string)
 {
 	char *str;
@@ -7,15 +15,26 @@ void print_s(va_list string)
 	str = va_arg(string, char*);
 	if (str == NULL)
 		str = "(null)";
-	for (index = 0; str[index] != 0; index++);
+	for (index = 0; str[index] != 0; index++)
 		_putchar(str[index]);
 }
+
+/**
+  *print_c - Function print character.
+  *@c: Argument type va_list.
+  *Return: Nothing.
+  *
+  */
+
 void print_c(va_list c)
 {
 	char ch = va_arg(c, int);
+
 	_putchar(ch);
 }
-/**void print_i(va_list i)
+
+/**
+  *void print_i(va_list i)
   *{
   *	write(1, va_arg(i, int), _strlen((char *)i));
   *}
